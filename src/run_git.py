31449ab592ab1ea0d0e684d2a34e8d06ef8bdd09
RutujaK2211 <92713680+RutujaK2211@users.noreply.github.com>
2023-01-26 21:02:59 +0530

    Add files via upload

---
+++ src/run_git.py
@@ -0,0 +1,39 @@
+import subprocess
+
+import github_action_utils as gha_utils  # type: ignore
+
+
+def configure_git_author(username: str, email: str) -> None:
+    """
+    Configure the git author.
+    """
+    with gha_utils.group("Configure Git Author"):
+        gha_utils.notice(f"Setting Git Commit User to '{username}'.")
+        gha_utils.notice(f"Setting Git Commit email to '{email}'.")
+
+        run_subprocess_command(["git", "config", "user.name", username])
+        run_subprocess_command(["git", "config", "user.email", email])
+
+
+def configure_safe_directory(directory: str) -> None:
+    """
+    Configure git safe.directory.
+    """
+    with gha_utils.group("Configure Git Safe Directory"):
+        run_subprocess_command(
+            ["git", "config", "--global", "--add", "safe.directory", directory]
+        )
+        
+def git_diff() -> str:
+    """Return the git diff"""
+    return subprocess.run(["git", "diff"], capture_output=True, text=True).stdout
+
+
+def run_subprocess_command(command: list[str]) -> None:
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    if result.returncode != 0:
+        gha_utils.error(result.stderr)
+        raise SystemExit(result.returncode)
+
+    gha_utils.echo(result.stdout)        
---
+++ src/utils.py
@@ -0,0 +1,29 @@
+from functools import cache
+
+import github_action_utils as gha_utils  # type: ignore
+import requests
+
+from .run_git import git_diff
+
+@cache
+def get_request_headers(github_token: str | None = None) -> dict[str, str]:
+    """Get headers for GitHub API request"""
+    headers = {"Accept": "application/vnd.github.v3+json"}
+
+    if github_token:
+        headers.update({"authorization": f"Bearer {github_token}"})
+
+    return headers
+
+def add_git_diff_to_job_summary() -> None:
+    """Add git diff to job summary"""
+    markdown_diff = (
+        "<details>"
+        "<summary>Git Diff</summary>"
+        f"\n\n```diff\n{git_diff()}```\n\n"
+        "</details>"
+    )
+    gha_utils.append_job_summary(markdown_diff)
+
+
+
