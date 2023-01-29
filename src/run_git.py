cd62cdc303a3306e7c66c23986c1f9c0663bf460
RutujaK2211 <92713680+RutujaK2211@users.noreply.github.com>
2023-01-29 22:58:35 +0530

    Update run_git.py

--- src/run_git.py
+++ src/run_git.py
@@ -3,16 +3,7 @@ import subprocess
 import github_action_utils as gha_utils  # type: ignore
 
 
-def configure_git_author(username: str, email: str) -> None:
-    """
-    Configure the git author.
-    """
-    with gha_utils.group("Configure Git Author"):
-        gha_utils.notice(f"Setting Git Commit User to '{username}'.")
-        gha_utils.notice(f"Setting Git Commit email to '{email}'.")
 
-        run_subprocess_command(["git", "config", "user.name", username])
-        run_subprocess_command(["git", "config", "user.email", email])
 
 
 def configure_safe_directory(directory: str) -> None:
@@ -23,6 +14,33 @@ def configure_safe_directory(directory: str) -> None:
         run_subprocess_command(
             ["git", "config", "--global", "--add", "safe.directory", directory]
         )
+
+
+
+
+def git_commit_changes(
+    commit_message: str,
+    commit_author: str,
+    commit_branch_name: str,
+    force_push: bool = False,
+) -> None:
+    """
+    Commit the changed files.
+    """
+    with gha_utils.group("Commit Changes"):
+        run_subprocess_command(["git", "add", "."])
+        run_subprocess_command(
+            ["git", "commit", f"--author={commit_author}", "-m", commit_message]
+        )
+        push_command = ["git", "push", "-u"]
+
+        if force_push:
+            push_command.append("-f")
+
+        push_command.extend(["origin", commit_branch_name])
+        run_subprocess_command(push_command)
+
+
 def git_has_changes() -> bool:
     """
     Check if there are changes to commit.
@@ -32,7 +50,8 @@ def git_has_changes() -> bool:
         return False
     except subprocess.CalledProcessError:
         return True
-        
+
+
 def git_diff() -> str:
     """Return the git diff"""
     return subprocess.run(["git", "diff"], capture_output=True, text=True).stdout
@@ -45,4 +64,4 @@ def run_subprocess_command(command: list[str]) -> None:
         gha_utils.error(result.stderr)
         raise SystemExit(result.returncode)
 
-    gha_utils.echo(result.stdout)        
+    gha_utils.echo(result.stdout)
